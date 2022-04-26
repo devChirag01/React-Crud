e83a9bd93a9f831f74bd05a9d4881d4e1279010a
Chirag <dev.chirag01@gmail.com>
2022-04-26 14:48:11 +0530

    Updates

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
 import React, { Component } from "react";
 import Table from "./table";
+import Form from "./form";
 
 class App extends Component {
   state = {
     characters: [
-      {
-        name: "Charlie",
-        job: "Janitora",
-      },
-      {
-        name: "Mac",
-        job: "Bouncer",
-      },
-      {
-        name: "Dee",
-        job: "Aspring actress",
-      },
-      {
-        name: "Dennis",
-        job: "Bartender",
-      },
+      // {
+      //   name: "Charlie",
+      //   job: "Janitora",
+      // },
+      // {
+      //   name: "Mac",
+      //   job: "Bouncer",
+      // },
+      // {
+      //   name: "Dee",
+      //   job: "Aspring actress",
+      // },
+      // {
+      //   name: "Dennis",
+      //   job: "Bartender",
+      // },
     ],
-    names: [
+    tableHeadings: [
       {
-        one: "Chirag",
+        one: "Users",
       },
     ],
   };
@@ -35,20 +36,28 @@ class App extends Component {
       characters: characters.filter((character, i) => {
         return i !== index;
       }),
-      names: {
-        one: "Addicted",
-      },
+      tableHeadings: [
+        {
+          one: "Employees",
+        },
+      ],
     });
   };
 
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+
   render() {
-    const { characters, names } = this.state;
+    const { characters, tableHeadings } = this.state;
     return (
       <div className="container">
+        <Form handleSubmit={this.handleSubmit} />
+
         <Table
           characterData={characters}
           removeCharacter={this.removeCharacter}
-          names={names}
+          tableHeadings={tableHeadings}
         />
       </div>
     );
